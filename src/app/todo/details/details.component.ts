57915f2e5ffd1431eae33972125f1f51eb7d359b
Prosenjit Manna <developer.prosenjit@gmail.com>
2017-05-09 22:35:52 +0530

    fix merge issue

--- src/app/todo/details/details.component.ts
+++ src/app/todo/details/details.component.ts
@@ -1,12 +1,7 @@
-<<<<<<< refs/remotes/origin/master
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-=======
+
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
 import { environment } from '../../../environments/environment';
->>>>>>> details view navigation added
 
 @Component({
   selector: 'app-todo-details',
@@ -14,22 +9,7 @@ import { environment } from '../../../environments/environment';
   styleUrls: ['./details.component.scss', '../todo.scss']
 })
 export class DetailsComponent implements OnInit {
-<<<<<<< refs/remotes/origin/master
-  public mode = true;
-  public params: any;
 
-  constructor(
-    private _acroute: ActivatedRoute
-  ) { }
-
-  ngOnInit() {
-    this._acroute.queryParams.subscribe(query => {
-      this.mode = query.mode;
-    });
-    this._acroute.params.subscribe(params => {
-      console.log(params);
-    });
-=======
   public mode;
   private subParams;
   private subQuery: any;
@@ -50,11 +30,10 @@ export class DetailsComponent implements OnInit {
    this.subQuery = this.route.queryParams.subscribe(query => {
     if (environment.debug) {
       console.log('query', query);
-    } 
+    }
     this.mode = query['mode'];
    });
 
->>>>>>> details view navigation added
   }
 
 }

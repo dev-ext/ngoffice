eb055ec402e2f48397168bf8d0f13a70efbafb78
Prosenjit Manna <developer.prosenjit@gmail.com>
2017-05-19 13:16:32 +0530

    archive added

--- src/app/todo/archive/archive.component.ts
+++ src/app/todo/archive/archive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'ngf-todo-archive',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./archive.component.scss', '../todo.scss']
 })
 export class ArchiveComponent implements OnInit {
+  tasks: any;
 
-  constructor() { }
+  constructor(
+    private _taskservice: TaskService
+  ) { }
 
   ngOnInit() {
+    const queryDone = {
+      orderByChild: 'done',
+      equalTo: true
+    }
+    this._taskservice.getQuery(queryDone)
+      .subscribe(res => {
+        this.tasks = res;
+      });
   }
 
 }
